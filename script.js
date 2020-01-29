d8629b6570e5ae8382c569f3ace3e6316a2e2f8f
Vishesh Singh <VisheshSingh16@gmail.com>
2020-01-29 10:17:29 -0600

    Check Length, Email & Password Match

--- script.js
+++ script.js
@@ -16,9 +16,13 @@ function showSuccess(input) {
   formEl.className = 'form-control success';
 }
 
-function isValid(email) {
+function checkEmail(input) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(String(input.value.trim()).toLowerCase())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
 function checkRequired(inputArr) {
@@ -35,9 +39,35 @@ function getFieldname(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+function checkLength(input, min, max) {
+  if (input.value.length < +min) {
+    showError(
+      input,
+      `${getFieldname(input)} must be atleast ${min} characters long`
+    );
+  } else if (input.value.length > +max) {
+    showError(
+      input,
+      `${getFieldname(input)} cannot be more than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkPasswords(input, input2) {
+  if (input.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
+}
+
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 30);
+  checkEmail(email);
+  checkPasswords(password, password2);
 });
