7dea9bd039fe415184bb59dbd1e57b4f84117c5f
Vishesh Singh <VisheshSingh16@gmail.com>
2020-01-29 09:47:50 -0600

    adding simple validation

---
+++ script.js
@@ -0,0 +1,58 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message) {
+  const formEl = input.parentElement;
+  formEl.className = 'form-control error';
+  const small = formEl.querySelector('small');
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formEl = input.parentElement;
+  formEl.className = 'form-control success';
+}
+
+function isValid(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else if (!isValid(email.value)) {
+    showError(email, 'Email is not valid');
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === '') {
+    showError(password2, 'Confirm password is required');
+  } else {
+    showSuccess(password2);
+  }
+
+  if (password.value !== password2.value) {
+    showError(password, 'Passwords do not match!');
+    showError(password2, 'Passwords do not match!');
+  }
+});
